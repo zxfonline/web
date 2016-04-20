aa90b2d4bd936769046a3a98d2a1b2532009f356
zxfonline <zxfonline@sina.com>
2016-04-20 21:26:20 +0800

    change
    change

--- examples/logger.go
+++ examples/logger.go
@@ -1,21 +1,17 @@
 package main
 
 import (
-    "github.com/hoisie/web"
-    "log"
-    "os"
+	"github.com/zxfonline/golog"
+
+	"github.com/zxfonline/web"
 )
 
 func hello(val string) string { return "hello " + val }
 
 func main() {
-    f, err := os.Create("server.log")
-    if err != nil {
-        println(err.Error())
-        return
-    }
-    logger := log.New(f, "", log.Ldate|log.Ltime)
-    web.Get("/(.*)", hello)
-    web.SetLogger(logger)
-    web.Run("0.0.0.0:9999")
+	golog.InitConfig("./log4go.cfg")
+	logger := golog.New("test")
+	web.Get("/(.*)", hello)
+	web.SetLogger(logger)
+	web.Run("0.0.0.0:9999")
 }
--- examples/streaming.go
+++ examples/streaming.go
@@ -1,24 +1,25 @@
 package main
 
 import (
-    "github.com/hoisie/web"
-    "net/http"
-    "strconv"
-    "time"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/zxfonline/web"
 )
 
 func hello(ctx *web.Context, num string) {
-    flusher, _ := ctx.ResponseWriter.(http.Flusher)
-    flusher.Flush()
-    n, _ := strconv.ParseInt(num, 10, 64)
-    for i := int64(0); i < n; i++ {
-        ctx.WriteString("<br>hello world</br>")
-        flusher.Flush()
-        time.Sleep(1e9)
-    }
+	flusher, _ := ctx.ResponseWriter.(http.Flusher)
+	flusher.Flush()
+	n, _ := strconv.ParseInt(num, 10, 64)
+	for i := int64(0); i < n; i++ {
+		ctx.WriteString("<br>hello world</br>")
+		flusher.Flush()
+		time.Sleep(1e9)
+	}
 }
 
 func main() {
-    web.Get("/([0-9]+)", hello)
-    web.Run("0.0.0.0:9999")
+	web.Get("/([0-9]+)", hello)
+	web.Run("0.0.0.0:9999")
 }
